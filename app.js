fe23230db6917b4a93c595fadb3d7bb0631cebb9
Thanos Melistas <thanos.melistas@gmail.com>
2018-05-27 12:01:05 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let arr = ['the_great_gatsby', 'to_kill_a_mockingbird', 'tell_me_a_riddle', 
+'great_expectations', 'all_quiet_on_the_western_front', 'war_and_peace',
+'the_heart_is_a_lonely_hunter', 'native_son', 'the_road', 'frankenstein'];
+
+console.log(arr);
